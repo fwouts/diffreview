f32d866de7e9bb2f17775bbd5600835094d39b5d
Francois Wouts <f@zenc.io>
2018-08-23 22:04:19 +1000

    Refactor tree entirely to use GitHub API v3 (much faster)
    This also fixes the issue of comparing against master. Instead, it smartly picks the latest merged ancestor.

--- src/api/github/diff.ts
+++ src/api/github/diff.ts
@@ -1,176 +1,210 @@
-import fetchPullRequest from "@/api/github/graphql/pullrequest";
-import { loadNode, loadRepository } from "@/api/github/loader";
+import Octokit from "@octokit/rest";
 
 export async function loadDiff(
   token: string,
   owner: string,
-  repository: string,
+  repo: string,
   pullRequestId: number
 ): Promise<UpdatedDirectory> {
-  const pullRequest = await fetchPullRequest(
-    token,
+  const octokit = new Octokit();
+  octokit.authenticate({
+    type: "token",
+    token
+  });
+  const pullRequestCommits = (await octokit.pullRequests.getCommits({
     owner,
-    repository,
-    pullRequestId
-  );
-  const oldTreeId = await loadRepository(
-    token,
-    owner,
-    repository,
-    pullRequest.repository.pullRequest.baseRefName
-  );
-  const newTreeId = await loadRepository(
-    token,
-    owner,
-    repository,
-    pullRequest.repository.pullRequest.headRefName
-  );
-  return loadUpdatedDirectory(token, oldTreeId, newTreeId);
-}
+    repo,
+    number: pullRequestId
+  })).data;
 
-async function loadUpdatedDirectory(
-  token: string,
-  oldId: string,
-  newId: string
-): Promise<UpdatedDirectory> {
-  console.log(`Loading nodes ${oldId} and ${newId}...`);
-  const oldNode = await loadNode(token, oldId);
-  const newNode = await loadNode(token, newId);
-  if (oldNode.kind !== "dir" || newNode.kind !== "dir") {
-    throw new Error();
-  }
-  const newEntries = Object.entries(newNode.entries).map(
-    async ([entryName, newEntry]): Promise<[string, DiffTreeEntry]> => {
-      const oldEntry = oldNode.entries[entryName];
-      if (!oldEntry) {
-        // New file or directory.
-        if (newEntry.type === "file") {
-          return [
-            entryName,
-            {
-              kind: "added-file",
-              newId: newEntry.id
-            }
-          ];
-        } else {
-          console.log("Loading from updated: directory " + entryName);
-          return [entryName, await loadAddedDirectory(token, newEntry.id)];
-        }
-      } else if (oldEntry.id === newEntry.id) {
-        // Unchanged file or directory.
-        if (oldEntry.type === "file") {
-          return [
-            entryName,
-            {
-              kind: "unchanged-file",
-              id: oldEntry.id
-            }
-          ];
-        } else {
-          return [
-            entryName,
-            {
-              kind: "unchanged-dir",
-              id: oldEntry.id
-            }
-          ];
-        }
-      } else {
-        // Updated file or directory.
-        if (oldEntry.type !== newEntry.type) {
-          // TODO: Handle this.
-        }
-        if (oldEntry.type === "file") {
-          return [
-            entryName,
-            {
-              kind: "updated-file",
-              oldId: oldEntry.id,
-              newId: newEntry.id
-            }
-          ];
-        } else {
-          return [
-            entryName,
-            await loadUpdatedDirectory(token, oldEntry.id, newEntry.id)
-          ];
-        }
+  // Figure out which commit sha to compare this pull requests to.
+  //
+  // This is important because, for example, the latest master may include a lot
+  // of other changes. We want to make sure we show the code tree as it is in that
+  // diff.
+  let diffAgainstCommitSha = pullRequestCommits[0].parents![0].sha!;
+  let lastCommitSha = pullRequestCommits[0].sha!;
+  for (const commit of pullRequestCommits) {
+    for (const parentCommit of commit.parents!) {
+      if (parentCommit.sha !== lastCommitSha) {
+        // This is a merge commit.
+        diffAgainstCommitSha = parentCommit.sha;
       }
     }
-  );
-  const entries: { [entryName: string]: DiffTreeEntry } = (await Promise.all(
-    newEntries
-  )).reduce(
-    (acc: { [entryName: string]: DiffTreeEntry }, [entryName, entry]) => {
-      acc[entryName] = entry;
-      return acc;
-    },
-    {}
-  );
-  for (const [entryName, oldEntry] of Object.entries(oldNode.entries)) {
-    if (!newNode.entries[entryName]) {
-      // Deleted file or directory.
-      if (oldEntry.type === "file") {
-        entries[entryName] = {
-          kind: "deleted-file",
-          oldId: oldEntry.id
-        };
-      } else {
-        entries[entryName] = {
-          kind: "deleted-dir",
-          oldId: oldEntry.id
-        };
-      }
-    }
+    lastCommitSha = commit.sha!;
   }
-  const updatedDirectory: UpdatedDirectory = {
-    kind: "updated-dir",
-    oldId,
-    newId,
-    entries
-  };
-  return updatedDirectory;
-}
 
-async function loadAddedDirectory(
-  token: string,
-  directoryId: string
-): Promise<AddedDirectory> {
-  const directory = await loadNode(token, directoryId);
-  if (directory.kind !== "dir") {
-    throw new Error();
-  }
-  const entries = Object.entries(directory.entries).map(
-    async ([entryName, entry]): Promise<
-      [string, AddedFile | AddedDirectory]
-    > => {
-      if (entry.type === "file") {
-        return [
-          entryName,
-          {
-            kind: "added-file",
-            newId: entry.id
-          }
-        ];
-      } else {
-        console.log("Loading from added: directory " + entryName);
-        return [entryName, await loadAddedDirectory(token, entry.id)];
-      }
-    }
-  );
-  return {
-    kind: "added-dir",
-    newId: directoryId,
-    entries: (await Promise.all(entries)).reduce(
-      (
-        acc: { [entryName: string]: AddedFile | AddedDirectory },
-        [entryName, entry]
-      ) => {
-        acc[entryName] = entry;
+  const diffAgainstTree: {
+    tree: GetTreeResponseItem[];
+  } = (await octokit.gitdata.getTree({
+    owner,
+    repo,
+    recursive: 1,
+    tree_sha: diffAgainstCommitSha
+  })).data;
+  const originalFiles = makeTree(
+    diffAgainstTree.tree.reduce(
+      (acc, curr) => {
+        if (curr.type === "blob") {
+          acc[curr.path] = curr;
+        }
         return acc;
       },
-      {}
+      {} as { [path: string]: GetTreeResponseItem }
     )
+  );
+
+  const pullRequestFiles = (await octokit.pullRequests.getFiles({
+    owner,
+    repo,
+    number: pullRequestId
+  })).data;
+  const modifiedFiles = makeTree(
+    pullRequestFiles.reduce(
+      (acc, curr) => {
+        acc[curr.filename] = curr;
+        return acc;
+      },
+      {} as { [path: string]: Octokit.GetFilesResponseItem }
+    )
+  );
+
+  return buildUpdatedDirectory(originalFiles, modifiedFiles);
+}
+
+// This is not provided by Octokit, so we do it instead.
+export interface GetTreeResponseItem {
+  mode: string;
+  path: string;
+  sha: string;
+  type: "tree" | "blob";
+  size?: number;
+  url: string;
+}
+
+export type OriginalTree = Tree<GetTreeResponseItem>;
+export type ModifiedTree = Tree<Octokit.GetFilesResponseItem>;
+
+export interface Tree<Item> {
+  [name: string]:
+    | { kind: "tree"; tree: Tree<Item> }
+    | { kind: "item"; item: Item };
+}
+
+function makeTree<Item>(items: { [path: string]: Item }): Tree<Item> {
+  const tree: Tree<Item> = {};
+  for (const [path, item] of Object.entries(items)) {
+    insertKey(tree, path, item);
+  }
+  return tree;
+}
+
+function insertKey<Item>(tree: Tree<Item>, path: string, item: Item) {
+  const slashPosition = path.indexOf("/");
+  if (slashPosition !== -1) {
+    const ancestorName = path.substr(0, slashPosition);
+    if (!tree[ancestorName]) {
+      tree[ancestorName] = {
+        kind: "tree",
+        tree: {}
+      };
+    }
+    const treeEntry = tree[ancestorName];
+    if (treeEntry.kind !== "tree") {
+      throw new Error(`Expected a tree, found ${tree[ancestorName].kind}`);
+    }
+    insertKey(treeEntry.tree, path.substr(slashPosition + 1), item);
+  } else {
+    tree[path] = { kind: "item", item };
+  }
+}
+
+function buildUpdatedDirectory(
+  originalFiles: OriginalTree,
+  modifiedFiles: ModifiedTree
+): UpdatedDirectory {
+  const diffEntries: { [entryName: string]: DiffTreeEntry } = {};
+  for (const [name, entry] of Object.entries(originalFiles)) {
+    const modified = modifiedFiles[name];
+    if (modified) {
+      // Was it updated or deleted?
+      if (entry.kind === "tree" && modified.kind === "tree") {
+        diffEntries[name] = buildUpdatedDirectory(entry.tree, modified.tree);
+      } else if (entry.kind === "item" && modified.kind === "item") {
+        if (modified.item.status === "modified") {
+          diffEntries[name] = {
+            kind: "updated-file"
+          };
+        } else if (modified.item.status === "removed") {
+          diffEntries[name] = {
+            kind: "deleted-file"
+          };
+        }
+        // Note: other statuses are "added" and "renamed".
+      }
+    } else {
+      if (entry.kind === "tree") {
+        diffEntries[name] = buildUnchangedDirectory(entry.tree);
+      } else {
+        diffEntries[name] = {
+          kind: "unchanged-file"
+        };
+      }
+    }
+  }
+  for (const [name, entry] of Object.entries(modifiedFiles)) {
+    if (!originalFiles[name]) {
+      // This is new.
+      if (entry.kind === "tree") {
+        diffEntries[name] = buildAddedDirectory(entry.tree);
+      } else {
+        diffEntries[name] = {
+          kind: "added-file"
+        };
+      }
+    }
+  }
+  return {
+    kind: "updated-dir",
+    entries: diffEntries
+  };
+}
+
+function buildAddedDirectory(modifiedFiles: ModifiedTree): AddedDirectory {
+  const addedEntries: { [entryName: string]: AddedFile | AddedDirectory } = {};
+  for (const [name, entry] of Object.entries(modifiedFiles)) {
+    if (entry.kind === "tree") {
+      addedEntries[name] = buildAddedDirectory(entry.tree);
+    } else {
+      addedEntries[name] = {
+        kind: "added-file"
+      };
+    }
+  }
+  return {
+    kind: "added-dir",
+    entries: addedEntries
+  };
+}
+
+function buildUnchangedDirectory(
+  originalFiles: OriginalTree
+): UnchangedDirectory {
+  const existingEntries: {
+    [entryName: string]: UnchangedFile | UnchangedDirectory;
+  } = {};
+  for (const [name, entry] of Object.entries(originalFiles)) {
+    if (entry.kind === "tree") {
+      existingEntries[name] = buildUnchangedDirectory(entry.tree);
+    } else {
+      existingEntries[name] = {
+        kind: "unchanged-file"
+      };
+    }
+  }
+  return {
+    kind: "unchanged-dir",
+    entries: existingEntries
   };
 }
 
@@ -186,28 +220,22 @@ export type DiffTreeEntry =
 
 export interface AddedFile {
   kind: "added-file";
-  newId: string;
 }
 
 export interface DeletedFile {
   kind: "deleted-file";
-  oldId: string;
 }
 
 export interface UpdatedFile {
   kind: "updated-file";
-  oldId: string;
-  newId: string;
 }
 
 export interface UnchangedFile {
   kind: "unchanged-file";
-  id: string;
 }
 
 export interface AddedDirectory {
   kind: "added-dir";
-  newId: string;
   entries: { [entryName: string]: AddedFile | AddedDirectory };
 }
 
@@ -218,12 +246,10 @@ export interface DeletedDirectory {
 
 export interface UpdatedDirectory {
   kind: "updated-dir";
-  oldId: string;
-  newId: string;
   entries: { [entryName: string]: DiffTreeEntry };
 }
 
 export interface UnchangedDirectory {
   kind: "unchanged-dir";
-  id: string;
+  entries: { [entryName: string]: UnchangedFile | UnchangedDirectory };
 }
